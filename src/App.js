c8cd854b79a1a5de2ebe340690abc9cbd8b5d586
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-04-23 12:01:24 -0300

    Functions and props to switch game phases created

--- src/App.js
+++ src/App.js
@@ -20,15 +20,28 @@ function App() {
   // Esse estado vai armazenar o array de palavras para o jogo
   const [words] = useState(wordsList);
 
-  console.log(words);
+  // função para iniciar o jogo 
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  // função para processar a letra de entrada (Finalizar jogo)
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  // reiniciar o jogo
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
 
   return (
     <div className="App">
 
       {/* Direcionando usuário para os componentes caso a condição seja true */}
-      {gameStage === 'start' && <StartScreen />}
-      {gameStage === 'game' && <Game />}
-      {gameStage === 'end' && <GameOver />}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
 }
