3a5cc378a19b598e7025764383ffb7e520b446c6
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-04-23 11:02:15 -0300

    Project created

---
+++ src/data/Words.js
@@ -0,0 +1,11 @@
+export const categorias = {
+    frutas: ["Maçã", "Banana", "Laranja", "Abacaxi", "Morango", "Uva", "Manga", "Pêssego"],
+    legumes: ["Cenoura", "Batata", "Beterraba", "Ervilha", "Abobrinha", "Chuchu", "Vagem", "Berinjela"],
+    bebidas: ["Água", "Suco", "Refrigerante", "Café", "Chá", "Leite", "Vinho", "Cerveja"],
+    doces: ["Chocolate", "Bolo", "Sorvete", "Bala", "Pudim", "Brigadeiro", "Torta", "Doce de leite"],
+    carnes: ["Frango", "Porco", "Peixe", "Linguiça", "Carne seca", "Fraldinha", "Costela"],
+    laticínios: ["Leite", "Queijo", "Iogurte", "Manteiga", "Requeijão"],
+    cereais: ["Arroz", "Feijão", "Aveia", "Milho", "Trigo", "Cevada", "Granola", "Quinoa"],
+    temperos: ["Sal", "Pimenta", "Alho", "Cebola", "Orégano", "Manjericão", "Colorau", "Cominho"],
+    limpeza: ["Detergente", "Sabão", "Amaciante", "Água sanitária", "Desinfetante", "Esponja", "Multiuso"]
+};
